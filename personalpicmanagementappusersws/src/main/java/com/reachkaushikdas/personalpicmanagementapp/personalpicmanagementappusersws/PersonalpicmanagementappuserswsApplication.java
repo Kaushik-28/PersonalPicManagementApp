889dda9b736f56eb1004d046191f166d3fab7f00
Kaushik Das <Kaushik.Das@Staples.com>
2021-09-17 17:10:38 +0530

    Initial commit

---
+++ personalpicmanagementappusersws/src/main/java/com/reachkaushikdas/personalpicmanagementapp/personalpicmanagementappusersws/PersonalpicmanagementappuserswsApplication.java
@@ -0,0 +1,13 @@
+package com.reachkaushikdas.personalpicmanagementapp.personalpicmanagementappusersws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalpicmanagementappuserswsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalpicmanagementappuserswsApplication.class, args);
+    }
+
+}
